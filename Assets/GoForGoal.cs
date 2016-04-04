3bc8040a2c91a6c418cf06c5c407b6e946a84bed
Iruyan_Zak <yuri.kazuna@gmail.com>
2016-04-04 14:56:58 +0900

    仮の移動コードを作成

---
+++ Assets/GoForGoal.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoForGoal : MonoBehaviour {
+    [SerializeField]
+    Vector3 goal;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        var direction = goal - transform.position;
+        GetComponent<CharacterController>().Move(direction.normalized*0.01f);
+	}
+}
